de19917c4c7da3972741aa6d0ebd98bad1245467
nicoenciso <121879206+nicoenciso@users.noreply.github.com>
2024-02-05 14:49:54 +0000

    v1

--- server.js
+++ server.js
@@ -61,3 +61,8 @@ app.listen(port, () => {
   console.log(`Server is listening in port ${port}`)
 })
 
+const http = require('http');
+
+setInterval(() => {
+  http.get('https://issue-reporting-api.onrender.com');
+}, 280000); // Realiza la solicitud cada 4 minutos y 40 segundos (280000 milisegundos)
